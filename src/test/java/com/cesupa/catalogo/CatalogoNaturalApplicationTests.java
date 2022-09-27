08bb4a39f383b70ee1953c5e68b010422e62263b
RCFranco <rodrigocf08@gmail.com>
2022-09-27 00:29:52 -0300

    Initial commit

---
+++ src/test/java/com/cesupa/catalogo/CatalogoNaturalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesupa.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoNaturalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
